fad20aaa7d37c85f25709dac8d95a1d143c6afe3
matheus <102304929+matheuskkj77@users.noreply.github.com>
2023-05-27 12:12:07 -0300

    Update script.js

--- cpf/script.js
+++ cpf/script.js
@@ -1,69 +1,25 @@
-/* function validateCPF(event) {
-
-    event.preventDefault();
-
-    const cpfInput = document.getElementById("cpfInput");
-    const cpfValidationResult = document.getElementById("cpfValidationResult");
-
-    const cpf = cpfInput.value.replace(/\D/g, '');
-
-    if (cpf.length !== 11) {
-        cpfValidationResult.textContent = "CPF inválido";
-        cpfValidationResult.style.color = "red"
-        event.preventDefault();
-        return false;
-    }
-
-    let sum = 0;
-    for (let i = 0; i < 9; i++) {
-        sum += parseInt(cpf[i]) * (10 - i);
-    }
-
-    let digit1 = 0;
-    if (sum % 11 > 1) {
-        digit1 = 11 - (sum % 11);
-    }
-
-    if (parseInt(cpf[9]) !== digit1) {
-        cpfValidationResult.textContent = "CPF inválido";
-        cpfValidationResult.style.color = "red"
-        event.preventDefault();
-        return false;
-    }
-
-    sum = 0;
-    for (let i = 0; i < 10; i++) {
-        sum += parseInt(cpf[i]) * (11 - i);
-    }
-
-    let digit2 = 0;
-    if (sum % 11 > 1) {
-        digit2 = 11 - (sum % 11);
-    }
-
-    if (parseInt(cpf[10]) !== digit2) {
-        cpfValidationResult.textContent = "CPF inválido";
-        cpfValidationResult.style.color = "red"
-        event.preventDefault()
-        return false;
-    }
-
-    cpfValidationResult.textContent = "CPF válido";
-    cpfValidationResult.style.color = "green"
-    return true;
-} */
-
 function is_cpf (c) {
 
     if((c = c.replace(/[^\d]/g,"")).length != 11)
       return false
-  
-    if (c == "00000000000")
+    
+    /* elimina CPFs invalidos conhecidos */
+    if (c == "00000000000" ||
+        c == "11111111111" ||
+        c == "22222222222" ||
+        c == "33333333333" ||
+        c == "44444444444" ||
+        c == "55555555555" ||
+        c == "66666666666" ||
+        c == "77777777777" ||
+        c == "88888888888" ||
+        c == "99999999999")
       return false;
   
     var r;
     var s = 0;
-  
+    
+    /* valida o primeiro digito */
     for (i=1; i<=9; i++)
       s = s + parseInt(c[i-1]) * (11 - i);
   
@@ -75,6 +31,7 @@ function is_cpf (c) {
     if (r != parseInt(c[9]))
       return false;
   
+    /* valida o segundo digito */
     s = 0;
   
     for (i = 1; i <= 10; i++)
@@ -91,7 +48,7 @@ function is_cpf (c) {
     return true;
   }
   
-  
+    /* mascara */
   function fMasc(objeto,mascara) {
   obj=objeto
   masc=mascara
